bd50d467b32bcaef7dfb3ebeb80d5ef8fc415528
flipflop02 <florent.pokemon.soler9@gmail.com>
2022-10-25 14:48:42 +0200

    Routing

--- time_manager/lib/time_manager_web/router.ex
+++ time_manager/lib/time_manager_web/router.ex
@@ -4,4 +4,23 @@ defmodule TimeManagerWeb.Router do
   pipeline :api do
     plug :accepts, ["json"]
   end
+
+  scope "/api", TimeManagerWeb do
+    pipe_through :api
+    resources "/users", UserController, except: [:new, :edit, :index]
+    get "/users", UserController, :getByInfo , email: "XXX", username: "YYY"
+    get "/workingtimes", WorkingTimeController, :getAll, start: "XXX", end: "YYY"
+    scope "/workingtimes" do
+      post "/:userID", WorkingTimeController, :create
+      scope "/:userID" do
+        get "/:id", WorkingTimeController, :get
+      end
+    end
+    resources "/workingtimes", WorkingTimeController, only: [:update, :delete]
+    scope "/clocks" do
+      get "/:userID", ClockController, :show
+      post "/:userID", ClockController, :create
+    end
+  end
+
 end
