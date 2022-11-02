ab57d870fb16e5955f502b6a82257210d8ec4521
flipflop02 <florent.pokemon.soler9@gmail.com>
2022-11-02 09:43:40 +0100

    mise à jour

--- front-time-manager/src/main.js
+++ front-time-manager/src/main.js
@@ -1,6 +1,25 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import {   LayoutPlugin, 
+    ModalPlugin,
+    BVModalPlugin,
+    BNavbar,
+    BNavbarNav,
+    BNavItem,
+    BNavbarBrand,
+    BNavbarToggle,
+    BNavForm,
+    BCollapse,
+    BForm,
+    BAlert,
+    BModal,
+    BButton,
+    BProgress,
+    BVToastPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-import './assets/main.css'
-
-createApp(App).mount('#app')
+const app = createApp(App)
+//app.use(BootstrapVue)
+//app.use(IconsPlugin)
+app.mount('#app')
