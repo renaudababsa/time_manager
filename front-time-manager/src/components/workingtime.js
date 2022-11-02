1971a13c57fcab2f338e52c5eb9b71cc02a8fc38
Iliam Amara <iliam.amara@epitech.eu>
2022-11-02 14:12:59 +0100

    timetables

--- front-time-manager/src/components/workingtime.js
+++ front-time-manager/src/components/workingtime.js
@@ -36,10 +36,21 @@ export async function deleteWorkingTime(userId, id) {
   return result;
 }
 
-export async function getWorkingTime(userId) {
+export async function getId(username) {
   let result;
-  api(
-    '/api/workingtimes/1?start=2022-10-06 11:15:11&end=2023-10-06 11:11:11',
+  await api(
+    `/api/users/?username=${username}`,
+    'GET'
+  ).then((res) => {
+    result = res['data'];
+  });
+  return result[0].id;
+}
+
+export async function getWorkingTime(userId, start_date, end_date) {
+  let result;
+  await api(
+    `/api/workingtimes/${userId}?start=${start_date}&end=${end_date}`,
     'GET'
   ).then((res) => {
     result = res['data'];
