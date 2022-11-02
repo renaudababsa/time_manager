fd11452b032ac778b922f626bea0027387411f57
flipflop02 <florent.pokemon.soler9@gmail.com>
2022-11-02 09:53:44 +0100

    migrations group

---
+++ time_manager/priv/repo/migrations/20221102085254_create_groups.exs
@@ -0,0 +1,11 @@
+defmodule TimeManager.Repo.Migrations.CreateGroups do
+  use Ecto.Migration
+
+  def change do
+    create table(:groups) do
+      add :name, :string
+
+      timestamps()
+    end
+  end
+end
